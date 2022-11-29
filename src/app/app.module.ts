f4c1f9f44c719c94c5900231d3aa484a71a2f36b
Apr97 <pruthvirajausula@gmail.com>
2022-11-29 23:30:50 +0530

    projetc details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { ChecklistScreenComponent } from './checklist-screen/checklist-screen.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    LoginScreenComponent,
+    ChecklistScreenComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
